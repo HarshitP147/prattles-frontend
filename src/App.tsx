179c924ad4cd39c1443c85de401b51dd46ca5fcb
Harshit Pandit <harshit7757@gmail.com>
2024-05-31 18:17:20 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    if (import.meta.env.MODE === "development") {
+        // if the app is in production, the user shall see this
+
+        return (
+            <main className='h-[100vh] w-full bg-primary select-none'>
+                <h1 className='text-base-100 text-[2.2em] text-center pt-24'>MERN Chat app</h1>
+                <p className='text-white text-center'>A simple chat application built with MERN stack. This site is built with Typescript React and Daisy UI</p>
+                <p className='text-white text-center'>You can track the progress here:</p>
+
+                <h1 className='text-white text-[2em] text-center pt-44'>Count: { count }</h1>
+                <div className=' w-[20em] mx-auto mt-10 py-2 px-4 flex justify-between'>
+                    <button onClick={ () => setCount(value => value - 1) } className='btn btn-accent scale-125'>Decrease</button>
+                    <button onClick={ () => setCount(value => value + 1) } className='btn btn-accent scale-125'>Increase</button>
+                </div>
+            </main>
+        )
+    }
+
+
+    // if in development, we see this
+    return (
+        <>
+            <main>
+                <h1>Working for chat app</h1>
+            </main>
+        </>
+    )
+
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+        darktheme: {
+          "primary": "#1a1a1a", // Black shade
+          "secondary": "#1e90ff", // Blue shade
+          "accent": "#8a2be2", // Violet shade
+          "neutral": "#2f2f2f", // Medium dark shade
+          "base-100": "#ffffff", // White for text
+          "info": "#00cfff", // Light blue
+          "success": "#84cc16", // Green
+          "warning": "#ffc300", // Yellow
+          "error": "#dc2626", // Red
+        },
+      }
+    ]
+  }
+}
